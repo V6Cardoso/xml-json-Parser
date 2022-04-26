31aac3d488609aa05c3cdbd5cab0071f9ed412b1
V6Cardoso <59899335+V6Cardoso@users.noreply.github.com>
2022-04-26 17:17:44 -0300

    formatação json em andamento

--- src/App.java
+++ src/App.java
@@ -13,26 +13,61 @@ import javax.swing.GroupLayout.Group;
 public class App {
     public static void main(String[] args) throws Exception {
         Path path = Paths.get("input3.xml");
-        byte[] bytes = Files.readAllBytes(path);
+        //byte[] bytes = Files.readAllBytes(path);
 
-
+        StringBuilder file = new StringBuilder();
+        file.append(Files.readAllLines(path));
+        String s2 = file.toString();
 
         Pattern openBracket = Pattern.compile("<(\\w+)+ ?([\\w ]+)?>");
         Pattern brackets = Pattern.compile("</?(.+?)>");
         
         
         
-        List<Objeto> root = ConteudoDoObjeto(bytes, 0, bytes.length);
+        List<Objeto> root = ConteudoDoObjeto(file);
+        //PrintaObjetos(root);
         
-        System.out.println(root);
+        Path saida = Paths.get("output.json");
+        StringBuilder build = new StringBuilder();
+
+        build = MontaObjeto(root);
+
+        byte[] bytes = build.toString().getBytes();
+        Files.write(saida, bytes);
     }
 
-    public static void MontaJSON(){
+    public static void PrintaObjetos(List<Objeto> objetos){
+        System.out.println();
+        for (Objeto objeto : objetos) {
+            System.out.println(objeto.nome);
+            System.out.println("Propriedades:");
+            for (Propriedade p : objeto.propriedades) {
+                System.out.println("chave: " + p.valor + " valor: " + p.valor);
+            }
+            System.out.println("Objetos:");
+            PrintaObjetos(objeto.filho);
+            System.out.println();
+        }
+    }
+
+    public static StringBuilder MontaObjeto(List<Objeto> objetos){
         StringBuilder json = new StringBuilder();
-        
+        json.append("{ \n\t");
+        for (Objeto objeto : objetos) {
+            json.append("\"" + objeto.nome + "\"");
+            if(objeto.propriedades.size() != 0){
+                json.append(" : ");
+                json.append(objeto.propriedades.get(0).valor);
+            }
+            else{
+                json.append(MontaObjeto(objeto.filho));
+            }
+        }
+        json.append("\n\t}");
+        return json;
     }
 
-    public static List<Objeto> ConteudoDoObjeto(byte[] bytes, int inicio, int fim){
+    public static List<Objeto> ConteudoDoObjeto(StringBuilder text){
 
         Pattern bothBrackets = Pattern.compile("<(.+?)>(.+)</?(.+?)>", Pattern.DOTALL);
         Pattern property = Pattern.compile("[^<>]*");
@@ -41,8 +76,10 @@ public class App {
 
         StringBuilder xmlString = new StringBuilder();
 
-        for(int i = inicio; i < fim; i++){
-            xmlString.append((char) bytes[i]);
+        for(int i = 0; i < text.length(); i++){
+            String s1 = text.toString();
+            xmlString.append(text.toString().charAt(i));
+            String s = xmlString.toString();
             Matcher m = bothBrackets.matcher(xmlString);
             if(m.find() && m.group(1).equals(m.group(3))){
 
@@ -51,14 +88,15 @@ public class App {
 
                 Matcher n = property.matcher(m.group(2).toString());
 
-                if(n.find()){
+                if(n.matches()){
                     Propriedade prop = new Propriedade();
                     prop.chave = m.group(1);
                     prop.valor = m.group(2);
                     objeto.propriedades.add(prop);
                 }
                 else{
-                    objeto.filho = ConteudoDoObjeto(bytes, xmlString.length(), i);
+                    StringBuilder novoTexto = new StringBuilder(m.group(2));
+                    objeto.filho = ConteudoDoObjeto(novoTexto);
                 }
                 objetos.add(objeto);
                 xmlString.setLength(0);
